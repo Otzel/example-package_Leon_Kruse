164d68ee330d87bc531c8b195c1374b261f005c0
Leon Kruse <82497650+Otzel@users.noreply.github.com>
2025-07-01 10:52:41 +0200

    Add add_three function and test

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Add src directory to path
 sys.path.insert(0, str(Path(__file__).resolve().parents[1] / 'src'))
 
-from example_package_Leon_Kruse.example import add_one, add_two
+from example_package_Leon_Kruse.example import add_one, add_two, add_three
 
 
 def test_add_one():
@@ -12,3 +12,6 @@ def test_add_one():
 
 def test_add_two():
     assert add_two(3) == 5
+
+def test_add_three():
+    assert add_three(3) == 6
