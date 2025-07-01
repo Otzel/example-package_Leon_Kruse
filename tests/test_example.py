bfd2ec480c3806912b21a84f3f8a6d2686c964b0
Leon Kruse <leon.kruse@tuhh.de>
2025-07-01 10:04:36 +0200

    initial tutorial setup

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,8 +4,11 @@ from pathlib import Path
 # Add src directory to path
 sys.path.insert(0, str(Path(__file__).resolve().parents[1] / 'src'))
 
-from example_package_Leon_Kruse.example import add_one
+from example_package_Leon_Kruse.example import add_one, add_two
 
 
 def test_add_one():
     assert add_one(3) == 4
+
+def test_add_two():
+    assert add_two(3) == 5
